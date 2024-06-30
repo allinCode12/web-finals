5480f7fcda0063a2dc270b1b65494fa5baaece59
Patrick <mesinapatrick@gmail.com>
2024-06-30 13:55:01 +0800

    Header and footer update

--- website_structure/js/scripts.js
+++ website_structure/js/scripts.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+    
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+});
+
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
@@ -38,8 +52,8 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: true,
+        autoplaySpeed: 3000,
+        arrows: false,
         dots: false,
         responsive: [
             {
